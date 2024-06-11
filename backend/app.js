c46d791484d7e367d1ff7e42aec6aa9d423ed10b
Sabarish <116443854+Sabarish0206@users.noreply.github.com>
2024-06-11 11:53:13 +0530

    Edited turf operation

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user-routes.js';
+import adminRouter from './routes/admin-routes.js';
+import turfRouter from './routes/turf-routes.js';
 dotenv.config();
 const app = express();
+
+
+//middlewares
+app.use(express.json());
+app.use("/user",userRouter);
+app.use("/admin",adminRouter);
+app.use("/turf",turfRouter);
 
 
 mongoose.connect(
