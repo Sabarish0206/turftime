fdb5e6e3ddb8705fb90085f407a8b863f2784965
Sabarish <116443854+Sabarish0206@users.noreply.github.com>
2024-07-07 09:23:27 +0530

    Ready to deploy backend

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,34 @@ import adminRouter from './routes/admin-routes.js';
 import turfRouter from './routes/turf-routes.js';
 import bookingRouter from './routes/booking-routes.js';
 import cors from 'cors';
-
 dotenv.config();
-
 
 const app = express();
 
+let allowedOrigins;
+if (process.env.NODE_ENV === 'development') {
+  allowedOrigins = [process.env.FRONTEND_URL];
+} else if (process.env.NODE_ENV === 'production') {
+  allowedOrigins = [process.env.FRONTEND_URL];
+} else {
+  allowedOrigins = [];
+}
 
-
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback) => {
+    //   does not allow request with no origin (postman like tools)
+    //   if (process.env.NODE_ENV === 'production' && !origin) {
+    //     const msg = 'The CORS policy for this site does not allow access from requests without an origin.';
+    //     return callback(new Error(msg), false);
+    //   }
+      if (process.env.NODE_ENV === 'production' && !allowedOrigins.includes(origin)) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    optionsSuccessStatus: 200
+  }));
 
 
 //middlewares
@@ -28,7 +47,7 @@ mongoose.connect(
     `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.oxuhthb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 )
 .then(()=>
-    app.listen(5000,()=>
+    app.listen(process.env.BACKEND_PORT || 5000,()=>
         console.log("Connected to DB & server is running")
     )
 )
